68e8dd21538356eaed1fa6e6e5b93e39784752a8
Ellysha Chavez <ellyshachavez@gmail.com>
2019-03-30 11:59:48 -0700

    utd

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,12 @@
 // Dependencies
 var friends = require("../data/friends.js");
 
-module.exports = function(app) {
-  
-    app.get("/api/friends", function (req, res) {
-      res.json(friends);
-    });
-  
-
+// ROUTING
+//  API GET request will display friends when the user visits the page.
+module.exports = function (app) {
+  app.get('/api/friends', function (req, res) {
+    res.json(friends);
+  });
 
   // API POST request submit survey data into our friends.js friendsArray.
   app.post("/api/friends", function (req, res) {
