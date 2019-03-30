26a03ff788b689c75c894a9adc13b446507d838b
Ellysha Chavez <ellyshachavez@gmail.com>
2019-03-29 22:49:26 -0700

    paths

---
+++ app/data/friend.js
@@ -0,0 +1,55 @@
+var friendsArray = [
+    {
+        name: "George of the Jungle",
+        photo: "https://vignette.wikia.nocookie.net/disney/images/6/64/George_of_the_Jungle_Waving.jpg/revision/latest?cb=20120406072510",
+        scores: [1,3,2,4,5,4,4,1,3,2]
+    },
+    {
+        name: "Buffy Summers",
+        photo: "https://static.giantbomb.com/uploads/scale_small/3/39164/2865332-2722260477-buffy.jpg",
+        scores: [2,5,3,2,5,1,3,2,5]
+    },
+    {
+        name: "ET",
+        photo: "https://i.ytimg.com/vi/f6sR9R3k_kA/maxresdefault.jpg",
+        scores: [4,3,1,3,3,4,1,1,1,3]
+    },
+    {
+        name: "The Doctor",
+        photo: "https://cdn3.whatculture.com/images/2013/03/matt-smith_doctor_who11.jpg",
+        scores: [5,5,1,5,1,5,1,1,4,5]
+    },
+    {
+        name: "Rick Sanchez",
+        photo: "https://vignette.wikia.nocookie.net/rickandmorty/images/a/a6/Rick_Sanchez.png/revision/latest/scale-to-width-down/620?cb=20160923150728",
+        scores: [1,1,1,3,1,1,1,5,1,5]
+    },
+    {
+        name: "Leslie Knope",
+        photo: "https://tribzap2it.files.wordpress.com/2016/11/amy-poehler-leslie-knope-parks-and-recreation.jpg",
+        scores: [5,5,1,5,1,5,1,1,4,5]
+    },
+    {
+        name: "Terry Jeffords",
+        photo: "http://reductress.com/wp-content/uploads/2018/06/terry-crews.png",
+        scores: [2,3,2,2,5,4,1,5,1,2]
+    },
+    {
+        name: "Frodo Baggins",
+        photo: "https://www.google.com/imgres?imgurl=http%3A%2F%2Fimages2.fanpop.com%2Fimages%2Fphotos%2F7800000%2FFrodo-Baggins-frodo-7808554-600-450.jpg&imgrefurl=http%3A%2F%2Fwww.fanpop.com%2Fclubs%2Ffrodo%2Fimages%2F7808554%2Ftitle%2Ffrodo-baggins-photo&docid=F0uLDvhum8T-7M&tbnid=UNbYPjhezwJ-tM%3A&vet=10ahUKEwj5hdLhjanhAhXU7Z4KHfU6DyUQMwh5KA4wDg..i&w=600&h=450&bih=860&biw=1504&q=frodo%20baggins&ved=0ahUKEwj5hdLhjanhAhXU7Z4KHfU6DyUQMwh5KA4wDg&iact=mrc&uact=8",
+        scores: [4,2,1,4,2,5,3,2,5,5]
+    },
+    {
+        name: "Gary Gygax",
+        photo: "http://cdn.themis-media.com/media/global/images/galleries/display/47/47451.jpg",
+        scores: [4,4,4,3,2,5,1,4,5,5]
+    },
+    {
+        name: "Peter Parker",
+        photo: "https://orig00.deviantart.net/d423/f/2017/211/1/e/a_night_to_remember__peter_parker_x_reader__2_3_by_savrom-dbi8qtv.jpg",
+        scores: [4,3,5,4,4,3,1,3,1,2]
+    }
+];
+
+//Exports Friends Object 
+module.exports = friendsArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,41 @@
+// Dependencies
+var friends = require('../data/friends.js');
+
+// ROUTING
+//  API GET request displays friends when the user visits the page.
+module.exports = function (app) {
+  app.get('/api/friends', function (req, res) {
+    res.json(friends);
+  });
+
+  // API POST request submit survey data into friends.js friendsArray.
+  app.post('/api/friends', function (req, res) {
+
+    var bestMatch = {
+      name: "",
+      photo: "",
+      friendDifference: 1000
+    };
+
+    var userData = req.body;
+    var userScores = userData.scores;
+    var totalDifference = 0;
+
+    for (var i = 0; i < friends.length; i++) {
+      totalDifference = 0;
+
+      for (var j = 0; j < friends[i].scores; j++) {
+        totalDifference += Math.abs(friends[i].scores[j] - userScores[j]);
+      }
+
+      // if lowest mathematical difference, update friend variables
+      if (totalDifference <= bestMatch.friendDifference) {
+        bestMatch.name = friends[i].name;
+        bestMatch.photo = friends[i].photo;
+        bestMatch.friendDifference = totalDifference;
+      }
+    }
+    friends.push(userData);
+    res.json({ status: 'ok', name: bestMatch.name, photo: bestMatch.photo });
+  });
+};
