d4834cff2eafbdc7fb004b7c59189072990f7cca
Ellysha Chavez <ellyshachavez@gmail.com>
2019-03-30 09:18:44 -0700

    utd

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(bodyParser.text());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
